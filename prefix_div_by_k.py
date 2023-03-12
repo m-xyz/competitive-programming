4d43c20b7e991e674efaa2d970ea28972bbc8de4
Miguel <blank0010b@gmail.com>
2023-03-12 18:38:27 +0000

    n subarrays div by k

--- prefix_div_by_k.py
+++ prefix_div_by_k.py
@@ -4,12 +4,12 @@ def subarraysDivByK(A, K):
     prefix = 0
     count = [1] + [0] * K
     for a in A:
-        print('current_prefix:',prefix)
         prefix = (prefix + a) % K
         res += count[prefix]
         count[prefix] += 1
-        print('\tcount:', count[prefix])
     return res
-
-subarraysDivByK([4,5,0,-2,-3,1], 5)
+A = [4,5,0,-2,-3,1]
+k = 5
+r = subarraysDivByK(A, k)
+print('\tNumber of subarrays in',A,'divisable by',k,':', r)
 
